9869b5febfbcbea829fd94bb3c0c09de9968d9e0
Abhishek <60239357+Mazbootlaunda@users.noreply.github.com>
2026-03-04 18:55:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 
-const music = document.getElementById("music");
-const btn = document.getElementById("music-btn");
+const music = document.getElementById("music")
+const btn = document.getElementById("music-btn")
+const icon = document.getElementById("icon")
 
-let playing = false;
+let playing = false
 
 btn.onclick = () => {
-  if(!playing){
-    music.play();
-    btn.innerText = "⏸";
-  } else {
-    music.pause();
-    btn.innerText = "🔊";
-  }
-  playing = !playing;
-};
+
+ if(!playing){
+  music.play()
+  icon.innerText = "❚❚"
+ }else{
+  music.pause()
+  icon.innerText = "▶"
+ }
+
+ playing = !playing
+}
+
+// scroll lantern parallax
+
+window.addEventListener("scroll", () => {
+
+ const scroll = window.scrollY
+
+ document.querySelectorAll(".lantern").forEach((lantern,i)=>{
+
+   lantern.style.transform =
+   "translateY(" + (scroll * (0.03 + i*0.02)) + "px)"
+
+ })
+
+})
