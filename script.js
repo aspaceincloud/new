3ab6fd449e4efda6de4168bc8137caf3c93fd63b
Abhishek <60239357+Mazbootlaunda@users.noreply.github.com>
2026-03-04 22:09:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 
-window.addEventListener("scroll",()=>{
+const music=document.getElementById("music")
+const btn=document.getElementById("musicBtn")
 
-const scroll=window.scrollY
-
-document.querySelectorAll(".lantern").forEach((lantern,i)=>{
-lantern.style.transform="translateY("+(scroll*(0.05+i*0.03))+"px)"
+btn.addEventListener("click",()=>{
+music.play()
 })
-
-})
-
 
 const target=new Date("April 19, 2026 19:00:00").getTime()
 
 setInterval(()=>{
-
 const now=new Date().getTime()
 const diff=target-now
-
 const days=Math.floor(diff/(1000*60*60*24))
 const hours=Math.floor((diff%(1000*60*60*24))/(1000*60*60))
 const mins=Math.floor((diff%(1000*60*60))/(1000*60))
-
 document.getElementById("timer").innerHTML=days+" Days "+hours+" Hours "+mins+" Minutes"
-
 },1000)
