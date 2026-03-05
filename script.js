2e4a4687c7dd1055c61b79210148285eff78a9f2
Abhishek <60239357+Mazbootlaunda@users.noreply.github.com>
2026-03-05 11:20:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
+
 const music=document.getElementById("music")
-const btn=document.getElementById("musicBtn")
-
-btn.addEventListener("click",()=>{
-music.play()
-})
-
-const sections=document.querySelectorAll(".section")
-
-const observer=new IntersectionObserver(entries=>{
-entries.forEach(entry=>{
-if(entry.isIntersecting){
-entry.target.classList.add("visible")
-}
-})
-})
-
-sections.forEach(section=>observer.observe(section))
+document.getElementById("musicBtn").onclick=()=>music.play()
 
 const target=new Date("April 19, 2026 19:00:00").getTime()
 
 setInterval(()=>{
+
 const now=new Date().getTime()
 const diff=target-now
+
 const days=Math.floor(diff/(1000*60*60*24))
 const hours=Math.floor((diff%(1000*60*60*24))/(1000*60*60))
 const mins=Math.floor((diff%(1000*60*60))/(1000*60))
+
 document.getElementById("timer").innerHTML=days+" Days "+hours+" Hours "+mins+" Minutes"
+
 },1000)
